4e472a9777a488f67de5fc86082bc05fe227ca3d
0990linian <1445060616@qq.com>
2019-01-22 01:43:52 +0000

    Add comments

--- pogo_data_generation/data_generation_with_speed.py
+++ pogo_data_generation/data_generation_with_speed.py
@@ -1,6 +1,6 @@
 ################################################################################
-# This script aims to use python to call matlab function gen_varying_velocity, 
-# in order to generate related pogo-inp files with different input parameters.
+# This script aims to generate data (emitter, receiver and timeline) and label 
+# (inclusion_speed) and store them into a database.
 ################################################################################
 import os
 import sqlite3
@@ -9,17 +9,32 @@ from get_hist_field_pogo import pogo_get_hist_field
 
 
 def main():
-	start_E = 150
-	end_E = 155
-	start_R = 5
-	end_R = 7
+	"""
+	Main function.
+
+	Description:
+		- Define the range of Young's modulus and circle radius.
+		- Define name of the database.
+		- Generate data and label.
+	"""
+	start_E, end_E = 50, 210
+	start_R, end_R = 5, 15
 	database = "pogo_circle.db"
 	db_connection = create_database(database)
 	matlab_engine = matlab.engine.start_matlab()
-	gen_pogo_inp_youngs_modulus_list(matlab_engine, db_connection, start_E, end_E, start_R, end_R)
+	generate_pogo_data(matlab_engine, db_connection, start_E, end_E, start_R, end_R)
 
 
 def create_database(database):
+	"""
+	Create a new database with the default inclusion_speed table.
+
+	Args:
+		database: a string input.  The name of the newly created database.
+
+	Returns:
+		db_connection: The connection to the database.
+	"""
 	if os.path.isfile(database):
 		os.remove(database)
 
@@ -35,7 +50,39 @@ def create_database(database):
 	return db_connection
 
 
-def gen_pogo_inp_youngs_modulus_list(matlab_engine, db_connection, start_E, end_E, start_R, end_R):
+def generate_pogo_data(
+		matlab_engine, 
+		db_connection, 
+		start_E, 
+		end_E, 
+		start_R, 
+		end_R
+	):
+	"""
+	Generate ultrasonic data using Pogo
+
+	Description:
+		- Run matlab function "gen_varying_velocity" with Young's modulus and 
+			radius inputs to generate pogo-inp files and corresponding 
+			inclusion_speed.
+		- Insert inclusion_speed into database, using pogo-inp name as index.
+		- Run Pogo remotely to get pogo-hist files.
+		- Extract ultrasonic data from pogo-hist files and write them into 
+			database.
+		- The input Young's modulus and radius are scaled, this is done for 
+			easier range running.
+
+	Args:
+		matlab_engine: The engine of matlab, used to call matlab functions.
+		db_connection: The connection to the database.
+		start_E: The scaled low-end Young's modulus.
+		end_E: The scaled high-end Young's modulus.
+		start_R: The scaled low-end circle radius.
+		end_R: The scaled high-end circle radius.
+
+	Returns:
+		None
+	"""
 	cursor = db_connection.cursor()
 
 	for E in range(start_E, end_E):
@@ -44,11 +91,15 @@ def gen_pogo_inp_youngs_modulus_list(matlab_engine, db_connection, start_E, end_
 			inclusion_speed = matlab_engine.gen_varying_velocity(
 				E * 10e8, R * 10e-4, pogo_name
 			)
-			cursor.execute("INSERT INTO inclusion_speed (table_name, speed) VALUES (?, ?)", (pogo_name, inclusion_speed))
+			cursor.execute(
+				"INSERT INTO inclusion_speed (table_name, speed) VALUES (?, ?)", 
+				(pogo_name, inclusion_speed)
+			)
 			pogo_get_hist_field(pogo_name + ".pogo-inp")
 			write_pogo_into_db(cursor, matlab_engine, pogo_name)
-
-	db_connection.commit()
+			os.remove("pogo_gen/" + pogo_name + ".pogo-inp")
+		
+		db_connection.commit()
 
 
 def write_pogo_into_db(cursor, matlab_engine, pogo_name):
@@ -57,21 +108,19 @@ def write_pogo_into_db(cursor, matlab_engine, pogo_name):
 
 	Description:
 		- For each Pogo history file generated, using matlab function view_hist 
-			to derive the time-magnitude data at emitter and receiver.
+			to derive the ultrasonic data at emitter and receiver.
 		- Create a table in the database for every Pogo file generated, directly 
 			using their names.
 		- Log the data into corresponding tables.
 
 	Args:
-		database: A string input.  The name of the database.
-		matlab_engine: A string input.  The engine used to run matlab functions.
-		pogo_file_list: A list input.  The list contains all generated Pogo 
-			files.
+		cursor: The database cursor, used to execute sql commands.
+		matlab_engine: The engine used to run matlab functions.
+		pogo_name: A string input. The name of pogo-related files.
 
 	Returns:
 		None
 	"""
-	# Run matlab file view_hist_function.m.
 	# data_info has elements representing data at every single time frame, 
 	# with structure [time, emitter_magnitude, receiver_magnitude]
 	data_info = matlab_engine.view_hist(pogo_name)
