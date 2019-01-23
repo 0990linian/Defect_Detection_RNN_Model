7317293d063a3cfa646c1c50ad1e79e8b8a9a6ef
0990linian <1445060616@qq.com>
2019-01-23 15:49:43 +0000

    Add comments

--- pogo_data_generation/create_train_test_data.py
+++ pogo_data_generation/create_train_test_data.py
@@ -1,9 +1,30 @@
+################################################################################
+# This script aims to generate training and testing data from the database.
+################################################################################
 import pickle
 import sqlite3
 from sklearn.model_selection import train_test_split
 
 
 def create_training_testing_data(database):
+	"""
+	Create training and testing data and save them to pickle.
+
+	Description:
+		- Define two cursors of the database, one moves in table 
+			inclusion_speed, one gathers data from other tables.
+		- Use the table name recorded in the inclusion_speed to find 
+			corresponding data.
+		- Record the data and label in two lists, make sure they are in 
+			corresponding positions.
+		- Shuffle and create the training and testing data, save them to pickle.
+
+	Args:
+		database: A string input.  The name of the database.
+
+	Returns:
+		None.
+	"""
 	X, y = [], []
 	db_connection = sqlite3.connect(database)
 	cursor_speed = db_connection.cursor()
